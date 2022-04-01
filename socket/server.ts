466e0cfc280230783df19657fbeb9e6c12d3fd6e
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-01 12:41:02 +0200

    refactor: remove console.log from server.ts

--- socket/server.ts
+++ socket/server.ts
@@ -3,7 +3,6 @@ import { Server } from "socket.io";
 export function setupHandlers(io: Server) {
   io.on("connection", (socket) => {
     let current: string;
-    console.log("WOOHOO");
     socket.emit("WELCOME MESSAGE", `User ${socket.id} Connected`);
     socket.on("new-message", ({ msg, activeRoom }) =>
       socket.broadcast.to(activeRoom).emit("new-message-from-server", msg)
